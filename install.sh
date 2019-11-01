343fa563be6dcdd7c2f49a690c2677d572d568b7
moqiaoduo <moqiaoduo@gmail.com>
2019-11-01 17:16:34 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,205 @@
+#!/bin/sh
+
+function Get_Dist_Name()
+{
+    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt'
+    else
+        DISTRO='unknow'
+    fi
+}
+
+function instdpec()
+{
+    if [ "$1" == "CentOS" ] || [ "$1" == "CentOS7" ];then
+        $PM -y install wget curl
+    elif [ "$1" == "Debian" ] || [ "$1" == "Raspbian" ] || [ "$1" == "Ubuntu" ];then
+        $PM update
+        $PM -y install wget curl
+    else
+        echo "The shell can be just supported to install v2ray on Centos, Ubuntu and Debian."
+        exit 1
+    fi
+}
+
+UUID=$(cat /proc/sys/kernel/random/uuid)
+v2_domain=""
+api_domain=""
+
+Get_Dist_Name
+echo "Your OS is $DISTRO"
+instdpec $DISTRO
+
+echo "1. Install V2Ray by official shell script"
+bash <(curl -L -s https://install.direct/go.sh)
+
+echo "2. Setting V2Ray to vmess+ws+Caddy"
+#Modify V2Ray Config
+cat > /etc/v2ray/config.json <<EOF
+{
+    "stats": {},    
+    "api": {
+        "services": [
+            "HandlerService",
+            "LoggerService",
+            "StatsService"
+        ],
+        "tag": "api"
+    },
+    "policy": {
+        "levels": {
+            "0": {
+                "statsUserUplink": true,
+                "statsUserDownlink": true
+            },
+            "1": {
+                "statsUserUplink": false,
+                "statsUserDownlink": true
+            },
+            "2": {
+                "statsUserUplink": true,
+                "statsUserDownlink": false
+            },
+            "3": {
+                "statsUserUplink": false,
+                "statsUserDownlink": false
+            }
+        },
+        "system": {
+            "statsInboundUplink": true,
+            "statsInboundDownlink": true
+        }
+    },
+    "inbound": {
+        "port": 10000,
+        "listen":"127.0.0.1",
+        "protocol": "vmess",
+        "settings": {
+            "clients": [
+                {
+                    "alterId": 64,
+                    "id": "${UUID}",
+                    "level": 0,
+                    "email": "admin@msknw.club"
+                }
+            ]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/misaka_network"
+            }
+        },
+        "tag": "proxy"
+    },
+    "inboundDetour": [
+        {
+            "listen": "127.0.0.1",
+            "port": 8848,
+            "protocol": "dokodemo-door",
+            "settings": {
+                "address": "127.0.0.1"
+            },
+            "tag": "api"
+        }
+    ],
+    "log": {
+        "loglevel": "debug"
+    },
+    "outbound": {
+        "protocol": "freedom",
+        "settings": {}
+    },
+    "routing": {
+        "settings": {
+            "rules": [
+                {
+                    "inboundTag": [
+                        "api"
+                    ],
+                    "outboundTag": "api",
+                    "type": "field"
+                }
+            ]
+        },
+        "strategy": "rules"
+    }
+}
+EOF
+#Install Caddy
+curl https://getcaddy.com | bash -s personal
+mkdir /etc/caddy
+touch /etc/caddy/Caddyfile
+chown -R root:www-data /etc/caddy
+mkdir /etc/ssl/caddy
+chown -R www-data:root /etc/ssl/caddy
+chmod 0770 /etc/ssl/caddy
+curl -s https://raw.githubusercontent.com/mholt/caddy/master/dist/init/linux-systemd/caddy.service -o /etc/systemd/system/caddy.service
+chown www-data:www-data /var/log/caddy
+#Set Caddy Proxy
+cat > /etc/caddy/v2ray <<EOF
+${v2_domain}
+{
+  log /var/log/caddy/v2ray.log
+  tls moqiaoduo@gmail.com
+  proxy /misaka_network localhost:10000 {
+    websocket
+    header_upstream -Origin
+  }
+}
+EOF
+
+echo "3. Install v2ray_proxy"
+wget -o /usr/bin/v2ray_proxy -c $v2ray_proxy_url
+#Set Caddy Proxy
+cat > /etc/caddy/v2ray <<EOF
+${api_domain}
+{
+  log /var/log/caddy/api.log
+  tls moqiaoduo@gmail.com
+  proxy / localhost:8080 {
+    header_upstream -Origin
+  }
+}
+EOF
+cat > /etc/systemd/system/v2ray-proxy.service <<EOF
+[Unit]
+Description=V2Ray HTTP API WEB Proxy
+After=network.target v2ray.service
+Wants=network.target v2ray.service
+
+[Service]
+Restart=on-failure
+Type=simple
+PIDFile=/run/v2ray.pid
+ExecStart=/usr/bin/v2ray_proxy
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "4. Run and test"
+systemctl daemon-reload
+systemctl enable caddy.service
+systemctl start v2ray.service
+systemctl enable caddy.service
+systemctl start caddy.service
